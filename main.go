61d2f9ff18cb7897e1badf6bbc2208810dc9460b
antoine prudhomme <antoine.prudhomme@spendesk.com>
2019-01-01 13:42:01 +0100

    Step 2: post message on slack

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import "github.com/robfig/cron"
+import "bytes"
 import "fmt"
 import "sync"
+import "net/http"
+import "io/ioutil"
+
+var internalWebhookUrl string = "https://hooks.slack.com/services/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"
 
 func blockForever() {
 	wg := sync.WaitGroup{}
@@ -17,7 +22,31 @@ func startDailyCron() {
 }
 
 func postDailyMessage() {
-	fmt.Println("Hello from Cron")
+	var jsonBody = []byte(`{"text": "Hello from Go!"}`)
+	req, err := http.NewRequest("POST", internalWebhookUrl, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		fmt.Println("An error happened while creating the request", err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("An error happened while posting the daily message", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("An error happened while reading the daily message response body", err)
+		return
+	}
+
+	fmt.Println(string(body[:]))
 }
 
 func main() {
